dd318c72eaa193ab65d36da15b47881b019dfb87
Jack Bransfield <bransfieldjack@gmail.com>
2018-08-26 08:40:00 +0000

    Temporarily removed SQL configurations to enable deployent to heroku.

--- app.py
+++ app.py
@@ -5,26 +5,6 @@ from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-
-#Gets the username from the MySql database in the cloud9 workspace. 
-username = os.getenv('C9_USER')
-
-
-#Connect to the MySql database.
-connection = pymysql.connect(host='localhost', user = username, password = '', db = 'recipe')
-
-
-try:
-    #Query the database. Using DictCursor ensures that the rows returned include column names.
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = " select * from users; "
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
-finally:
-    #Close the connection to the database.
-    connection.close()
-
     
 #Creates a route that directs us to our sites home page. 
 @app.route('/')
