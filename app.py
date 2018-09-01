2fa9a48653ec19533b3d473430fdb7499829b8b5
Jack Bransfield <bransfieldjack@gmail.com>
2018-09-01 02:28:12 +0000

    Added final styling and input form items for the add recipe page. Finalising fields.

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import os
-import pymysql
 from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://root:s!evan101@ds233212.mlab.com:33212/recipes-data-centric'
+
+
+mongo = PyMongo(app)
 
     
 #Creates a route that directs us to our sites home page. 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    images = os.listdir(os.path.join(app.static_folder, "images"))
+    return render_template('home.html', images = images, recipe_name=mongo.db.recipe_name.find())
     
     
-@app.route('/add_recipe')
+@app.route('/add_recipe', methods = ['POST', 'GET'])
 def add_recipe():
+    item = mongo.db.recipe_name
+    item.insert_one(request.form.to_dict())
     return render_template('add_recipe.html')
     
     
